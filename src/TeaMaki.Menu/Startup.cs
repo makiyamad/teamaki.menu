a39aede5427c3bd7717fb88ea1ba1642240029a7
dmakiyama <dmakiyama@rdisoftware.com>
2020-11-26 23:32:22 -0600

    Registering Service in Consul (Service Discovery) using IHostedService

--- src/TeaMaki.Menu/Startup.cs
+++ src/TeaMaki.Menu/Startup.cs
@@ -6,6 +6,12 @@ using Microsoft.Extensions.Hosting;
 using TeaMaki.Persistence;
 using AutoMapper;
 using TeaMaki.Menu.ProductAggregate;
+using System.Threading.Tasks;
+using System.Threading;
+using Consul;
+using Microsoft.Extensions.Options;
+using System;
+using Microsoft.Extensions.Logging;
 
 namespace TeaMaki.Menu
 {
@@ -24,6 +30,15 @@ namespace TeaMaki.Menu
             services.AddAutoMapper(typeof(MenuProfile));
             services.AddControllers();
             services.AddSwaggerGen();
+            services.AddSingleton<IHostedService, ConsulRegisterService>();
+            services.Configure<MenuConfiguration>(Configuration.GetSection("Menu"));
+            services.Configure<ConsulConfiguration>(Configuration.GetSection("Consul"));
+
+            var consulAddress = Configuration.GetSection("Consul")["Url"];
+
+            services.AddSingleton<IConsulClient, ConsulClient>(provider =>
+                new ConsulClient(config => config.Address = new Uri(consulAddress)));
+
             services.AddPersistenceLibrary(Configuration.GetSection("Database"));
         }
 
@@ -43,7 +58,8 @@ namespace TeaMaki.Menu
 
             app.UseSwagger();
             app.UseSwaggerUI(
-                ui => {
+                ui =>
+                {
                     ui.SwaggerEndpoint("swagger/v1/swagger.json", "Menu v1");
                     ui.RoutePrefix = "";
                 }
@@ -55,6 +71,70 @@ namespace TeaMaki.Menu
             });
         }
     }
+
+
+    public class ConsulConfiguration
+    {
+        public string Url { get; set; }
+    }
+
+    public class MenuConfiguration
+    {
+        public string Url { get; set; }
+        public string ServiceName { get; set; }
+        public string ServiceId { get; set; }
+    }
+
+    public class ConsulRegisterService : IHostedService
+    {
+        private IConsulClient _consulClient;
+        private ConsulConfiguration _consulConfiguration;
+        private MenuConfiguration _menuConfiguration;
+        private ILogger<ConsulRegisterService> _logger;
+
+        public ConsulRegisterService(IConsulClient consulClient,
+            IOptions<MenuConfiguration> menuConfiguration,
+            IOptions<ConsulConfiguration> consulConfiguration,
+            ILogger<ConsulRegisterService> logger)
+        {
+            _consulClient = consulClient;
+            _consulConfiguration = consulConfiguration.Value;
+            _menuConfiguration = menuConfiguration.Value;
+            _logger = logger;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            var menuUri = new Uri(_menuConfiguration.Url);
+
+            var serviceRegistration = new AgentServiceRegistration()
+            {
+                Address = menuUri.Host,
+                Name = _menuConfiguration.ServiceName,
+                Port = menuUri.Port,
+                ID = _menuConfiguration.ServiceId,
+                Tags = new[] { _menuConfiguration.ServiceName }
+            };
+
+            await _consulClient.Agent.ServiceDeregister(_menuConfiguration.ServiceId, cancellationToken);
+            await _consulClient.Agent.ServiceRegister(serviceRegistration, cancellationToken);
+
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                await _consulClient.Agent.ServiceDeregister(_menuConfiguration.ServiceId, cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error when trying to de-register", ex);
+            }
+        }
+    }
+
+
 
     public class MenuProfile : Profile
     {
